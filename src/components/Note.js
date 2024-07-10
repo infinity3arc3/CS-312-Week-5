5eb8e5a1dfc73f3369e48fada146d054a8c07c99
infinity3arc3 <infinity3arc3@gmail.com>
2024-07-10 13:38:55 -0700

    Upload Keeper

---
+++ src/components/Note.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+function Note() {
+  return (
+    <div class="note">
+      <h1>This is a Title</h1>
+      <p>These are the notes!</p>
+    </div>
+  );
+}
+
+export default Note;
